260b192ff8765dc6e18b169f826e75c8f99a605c
Marina Valiquette <marina.valiquette@gmail.com>
2018-04-27 17:00:51 -0400

    Change psql command to match Travis CI

--- db/seedData.js
+++ db/seedData.js
@@ -4,5 +4,6 @@ const exec = util.promisify(require('child_process').exec);
 
 module.exports = function(file) {
   console.log(process.env.TEST_DATABASE_URL);
-  return exec(`psql -f ${file} -d postgres://dev:volk0dav@localhost/noteful-test`);
+  return exec(`psql -f ${file} -d postgres://postgres:@localhost/noteful-test`);
+  // return exec(`psql -f ${file} -d postgres://dev:volk0dav@localhost/noteful-test`);
 };
